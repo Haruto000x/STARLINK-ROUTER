84f313d1709ce9b7d00ff7b77c9c8ec89815c45a
Haruto <haruto000x@gmail.com>
2026-01-09 20:53:30 +0630

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,5 @@ platform_ = platform.system().lower()
 
 if platform_ == "linux":
     __import__('starlink_linux')
-elif platform == "windows":
-    __import__('starlink_window')
 else:
     print('Your Device Not Support!')
